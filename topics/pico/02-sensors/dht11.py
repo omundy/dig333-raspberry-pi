edd8ba2c35aca774d585ae56169ae5441ff1f900
Owen Mundy <omundy@gmail.com>
2023-03-22 15:08:54 -0400

    Create dht11.py

---
+++ topics/pico/02-sensors/dht11.py
@@ -0,0 +1,10 @@
+from machine import Pin
+from time import sleep
+import dht
+sensor = dht.DHT11(Pin(2))
+while True:
+    sensor.measure()
+    temp = sensor.temperature()
+    hum = sensor.humidity()
+    print("Temperature: {}°C   Humidity: {:.0f}% ".format(temp, hum))
+    sleep(10)
